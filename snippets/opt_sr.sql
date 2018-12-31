7878c459de3d7dc432c96f7e27d1300dae5b252e
Stephan Biastoch <biastoch@corpshaus2.de>
2018-12-31 11:48:40 +0100

    final

--- snippets/opt_sr.sql
+++ snippets/opt_sr.sql
@@ -1,5 +1,6 @@
 <eval_recursive_scenario_sr(scenario)> :=
   SELECT 
-    c.in_1                                                                      AS in_1, 
-    <scenario.query[c.in_1 / $1, e.result / fib(scenario.callsites[1].arg_1)] > AS res 
+    c.in_1                                                         AS in_1, 
+    <scenario.query[c.in_1 / $1,
+                    e.result / fib(scenario.callsites[1].arg_1)] > AS res 
   FROM callgraph c INNER JOIN evaluation e ON (e.in_1) = (c.out_1)
